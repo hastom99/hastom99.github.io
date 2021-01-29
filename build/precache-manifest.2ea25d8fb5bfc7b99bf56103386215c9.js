4256f9cd6430aec7b6d1f78adfdff10b3166de0c
Thomas Munoz <thomm88@hotmail.com>
2021-01-29 02:53:35 -0300

    modules

---
+++ build/precache-manifest.2ea25d8fb5bfc7b99bf56103386215c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9826eaccb481c6ba7cd4",
+    "url": "/static/css/main.b39c550b.chunk.css"
+  },
+  {
+    "revision": "9826eaccb481c6ba7cd4",
+    "url": "/static/js/main.9826eacc.chunk.js"
+  },
+  {
+    "revision": "84103c17f6a4eb7c5c83",
+    "url": "/static/js/1.84103c17.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3d30d10f0d3d069c5ffa9ec0011425e2",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ef8915be2368fac9fbb9e673140fd356.js"
+  "/precache-manifest.2ea25d8fb5bfc7b99bf56103386215c9.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
